1ba0fbb1eeb0e1dc148d99e6bf15c1e0ce011f5d
Colt McCormick <coltmccormick@hotmail.com>
2018-03-03 14:13:15 -0600

    challenge1 WIP

--- main.js
+++ main.js
@@ -1,1 +1,31 @@
 //main.js
+
+function printToDom(stringToPrint, divId){
+    document.getElementById(divId).innerHTML += stringToPrint; 
+  }
+
+//Challenge1
+
+
+var players = [{ name: "Bob", scores: [10, 65] }, 
+            { name: "Bill", scores: [90, 5] }, 
+            { name: "Charlie", scores: [40, 55] }
+        ]; // Displays "Bill"
+
+for (var i =0; i < players.length; i++){
+    //sum of scores
+    players[i].total = 0; 
+    for (var j=0; j < players[i].scores.length; j++){
+        players[i].total += players[i].scores[j];
+    }
+     
+        //console.log('player: ', players[i].name)
+    }
+    for (var totals = 0; totals < players.length; totals++){
+        var maxTotal;
+        if(platyers[totals].total<=100){
+            
+        }
+    }
+    //console.log("total: ", players[i].total)
+}
